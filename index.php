1bda33cedaba00a9c02e2ced9009c5d92289893c
giagra91 <giagra91@gmail.com>
2022-04-22 14:58:51 +0200

    inzializzazione progetto

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <title>Php Dischi</title>
+</head>
+<body>
+    <?php
+        include __DIR__ . "/server/data.php";
+    ?>
+    
+
+
+
+    <script src="http://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
